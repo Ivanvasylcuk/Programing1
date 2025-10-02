e3a4dd9193a1be58ff3589213341d3f58a422173
Ivan Vasylchuk <vasylchuk_ak24@nuwm.edu.ua>
2025-10-02 13:50:34 +0300

    first commit

---
+++ Lab1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Slovo
+{
+    private string word;
+
+    public void Input()
+    {
+        Console.Write("Введіть слово: ");
+        word = Console.ReadLine();
+    }
+
+    public int CountDigits()
+    {
+        int count = 0;
+        foreach (char c in word)
+        {
+            if (char.IsDigit(c))
+                count++;
+        }
+        return count;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Slovo slovo = new Slovo();
+        slovo.Input();
+        int digits = slovo.CountDigits();
+        Console.WriteLine($"Кількість цифр у слові: {digits}");
+    }
+}
