180792fdb3ed1127f607b5dab1d67d60bc80d6cf
Ivan Vasylchuk <vasylchuk_ak24@nuwm.edu.ua>
2025-10-02 15:11:54 +0300

    Додав робочий код програми

--- Lab1/obj/Debug/net9.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net9.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3a4dd9193a1be58ff3589213341d3f58a422173")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
